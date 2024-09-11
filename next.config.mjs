80da5d32021f553721acd6fd1e5d5545ee1b1afc
purplemoonit <purplemoonit@gmail.com>
2024-09-12 01:29:46 +0400

    added image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.real-estate-manager.redberryinternship.ge",
+        port: "",
+        pathname: "/storage/agent_avatars/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
