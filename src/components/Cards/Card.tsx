21a7f9375ba1522ae13c4241cf625e198847f4d6
purplemoonit <purplemoonit@gmail.com>
2024-09-12 01:31:21 +0400

    card component

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Image from "next/image";
+import { ReactNode } from "react";
+import { Badge } from "../Badges";
+
+export default function Card({
+  children,
+  image,
+  is_rental,
+}: {
+  children: ReactNode;
+  image: string;
+  is_rental: boolean;
+}) {
+  return (
+    <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-2xl w-96">
+      <div className="relative h-56 overflow-hidden text-white rounded-md">
+        <Image src={image} fill={true} alt="Picture of the author" />
+      </div>
+      <Badge label={is_rental} className="absolute top-3 left-3" />
+      <div className="py-[22px] px-[25px]">{children}</div>
+    </div>
+  );
+}
