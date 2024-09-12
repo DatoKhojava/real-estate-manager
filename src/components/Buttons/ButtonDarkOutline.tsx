7320e897259c2e41fbf014dd56dd82173e1fae7a
purplemoonit <purplemoonit@gmail.com>
2024-09-12 15:52:28 +0400

    deleted junk

--- src/components/Buttons/ButtonDarkOutline.tsx
+++
@@ -1,7 +0,0 @@
-export default function ButtonDarkOutline({ label }: { label: string }) {
-  return (
-    <button className="bg-white text-[#808A93] border-[1px] border-[#808A93] hover:bg-[#909ba4] hover:text-white active:bg-[#717a82] active:text-white font-medium py-2 px-4 rounded-lg flex items-center space-x-2">
-      <span className="font-FiraGO">{label}</span>
-    </button>
-  );
-}
