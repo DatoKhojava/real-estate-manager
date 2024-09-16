4297b607314bfb3fae9a319e6f731d2e2dd506fd
purplemoonit <purplemoonit@gmail.com>
2024-09-16 23:27:38 +0400

    updated and created some components

--- src/components/Filter/FilterMenu.tsx
+++ src/components/Filter/FilterMenu.tsx
@@ -2,47 +2,134 @@
 
 import { useState } from "react";
 import { Dropdown } from "../Dropdown";
+import { Checkbox } from "../Checkbox";
+import PricesFilter from "./PricesFilter";
+import AreaFilter from "./AreaFilter";
+
+const regions = [
+  {
+    id: 1,
+    name: "გორი",
+  },
+  {
+    id: 2,
+    name: "თბილისი",
+  },
+  {
+    id: 3,
+    name: "გორი",
+  },
+  {
+    id: 4,
+    name: "სამეგრელო",
+  },
+  {
+    id: 5,
+    name: "კახეთი",
+  },
+  {
+    id: 6,
+    name: "გორი",
+  },
+  {
+    id: 7,
+    name: "აჭარა",
+  },
+  {
+    id: 8,
+    name: "აჭარა",
+  },
+  {
+    id: 9,
+    name: "აჭარა",
+  },
+  {
+    id: 10,
+    name: "აჭარა",
+  },
+  {
+    id: 11,
+    name: "აჭარა",
+  },
+  {
+    id: 12,
+    name: "აჭარა",
+  },
+];
 
 export default function FilterMenu() {
-  const [selectedOption, setSelectedOption] = useState<string>("");
+  //global state
+  const [selectedOption, setSelectedOption] = useState();
+  //Local States
+  const [regionList, setRegionsList] = useState<number[]>([]);
+  const [priceRange, setPriceRange] = useState<string>();
+  const [areaRange, setAreaRange] = useState<string>();
+  const [rooms, setRooms] = useState("");
 
-  const regionsOption = ["ქართლი", "კახეთი", "გურია", "აჭარა"];
-  const options = ["რაღაცა", "რუღაცა", "რაღაცა 1"];
 
-  const handleOptionSelect = (option: any) => {
-    setSelectedOption(option);
+  const regionsHandler = (region: { id: number; name: string }) => {
+    if (regionList.includes(region.id)) {
+      setRegionsList(regionList.filter((i) => i !== region.id));
+    } else {
+      setRegionsList((prev) => [...prev, region.id]);
+    }
   };
 
   return (
     <div className="border border-slate-300 w-fit rounded-[10px] flex gap-6 p-1">
       <Dropdown
-        options={regionsOption}
-        selectedOption={selectedOption}
-        onOptionSelect={handleOptionSelect}
+        selectedOption={regionList}
         label="რეგიონი"
         dropdownTitle="რეგიონის მიხედვით"
-      />
+      >
+        <div className="grid grid-cols-3 grid-rows-4 gap-4 w-[731px]">
+          {regions.map((item) => (
+            <div className="flex w-48" key={item.id}>
+              <Checkbox
+                isChecked={regionList.includes(item.id)}
+                onChange={() => regionsHandler(item)}
+                checkboxId={item.id.toString()}
+              />
+              <label
+                htmlFor={item.id.toString()}
+                className="ml-2 font-FiraGO text-[#021526] font-medium"
+              >
+                {item.name}
+              </label>
+            </div>
+          ))}
+        </div>
+      </Dropdown>
       <Dropdown
-        options={options}
-        selectedOption={selectedOption}
-        onOptionSelect={handleOptionSelect}
+        selectedOption={priceRange}
         label="საფასო კატეგორია"
-        dropdownTitle="რეგიონის მიხედვით"
-      />
+        dropdownTitle="ფასის მიხედვით"
+      >
+        <PricesFilter setPriceRange={setPriceRange} />
+      </Dropdown>
       <Dropdown
-        options={options}
-        selectedOption={selectedOption}
-        onOptionSelect={handleOptionSelect}
+        selectedOption={areaRange}
         label="ფართობი"
-        dropdownTitle="რეგიონის მიხედვით"
-      />
+        dropdownTitle="ფართობის მიხედვით"
+      >
+        <AreaFilter setAreaRange={setAreaRange} />
+      </Dropdown>
       <Dropdown
-        options={options}
-        selectedOption={selectedOption}
-        onOptionSelect={handleOptionSelect}
+        selectedOption={rooms}
         label="საძინებლების რაოდენობა"
-        dropdownTitle="რეგიონის მიხედვით"
-      />
+        dropdownTitle="საძინებლის მიხედვით"
+      >
+        <div className="mt-6">
+          <input
+            className="w-11 h-10 border rounded-md text-center"
+            name="rooms"
+            value={rooms}
+            onChange={(e) => setRooms(e.target.value)}
+            type="number"
+            placeholder="1"
+          />
+        </div>
+      </Dropdown>
     </div>
   );
 }
---
+++ src/components/Filter/PricesFilter.tsx
@@ -0,0 +1,131 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+interface PricesFilterProps {
+  setPriceRange: () => string;
+}
+
+export default function PricesFilter({ setPriceRange }: any) {
+  const [minPrice, setMinPrice] = useState("");
+  const [maxPrice, setMaxPrice] = useState("");
+  const [errorMessage, setErrorMessage] = useState(false);
+
+  useEffect(() => {
+    if (parseFloat(maxPrice) < parseFloat(minPrice)) setErrorMessage(true);
+    setPriceRange(`${minPrice}₾ - ${maxPrice}₾`);
+  }, [minPrice, maxPrice]);
+
+  return (
+    <div className="w-96 font-FiraGO">
+      <div className="flex gap-4">
+        <div className="relative">
+          <input
+            type="number"
+            name="minPrice"
+            placeholder="დან"
+            value={minPrice}
+            onChange={(e) => setMinPrice(e.target.value)}
+            className="w-full px-3 py-2 text-gray-700 bg-gray-50 border border-gray-300 rounded-md"
+          />
+          <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-black w-5 h-5">
+            ₾
+          </span>
+        </div>
+        <div className="relative ">
+          <input
+            type="number"
+            name="maxPrice"
+            placeholder="დან"
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(e.target.value)}
+            className="w-full px-3 py-2 text-gray-700 bg-gray-50 border border-gray-300 rounded-md"
+          />
+          <span className="absolute right-3 top-1/2 transform -translate-y-1/2 text-black w-5 h-5">
+            ₾
+          </span>
+        </div>
+      </div>
+      {errorMessage ? (
+        <div className="mt-6">
+          <label className="text-black font-FiraGO text-lg font-bold">
+            გთხოვთ შეიყვანოთ ვალიდური რიცხვები
+          </label>
+        </div>
+      ) : (
+        <div className="mt-6 flex w-[334px] gap-20">
+          <div className="w-[155px]">
+            <label className="font-bold text-lg">მინ. ფასი</label>
+            <div className="mt-4 text-base flex flex-col">
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMinPrice("50000")}
+              >
+                50,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMinPrice("100000")}
+              >
+                100,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMinPrice("150000")}
+              >
+                150,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMinPrice("200000")}
+              >
+                200,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMinPrice("300000")}
+              >
+                300,000 ₾
+              </label>
+            </div>
+          </div>
+          <div className="w-[155px]">
+            <label className="font-bold text-lg">მაქს. ფასი</label>
+            <div className="mt-4 text-base flex flex-col">
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMaxPrice("50000")}
+              >
+                50,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMaxPrice("100000")}
+              >
+                100,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMaxPrice("150000")}
+              >
+                150,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMaxPrice("200000")}
+              >
+                200,000 ₾
+              </label>
+              <label
+                className="cursor-pointer my-1 hover:text-slate-600"
+                onClick={() => setMaxPrice("300000")}
+              >
+                300,000 ₾
+              </label>
+            </div>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
